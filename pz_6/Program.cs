762c71954141b937bc5315c8a7a98b475941ea27
Алдияр Макимов <makimovaldiarsamatovich@gmail.com>
2022-09-26 13:32:28 +0500

    pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double y = double.Parse(Console.ReadLine());
+            double z = 0.0;
+            while (x <= y)
+            {
+                z = z + (1/x);
+                x ++;
+            }
+            Console.WriteLine(z);
+        }
+    }
+}
