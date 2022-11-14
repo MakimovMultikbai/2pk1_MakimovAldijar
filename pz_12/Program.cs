a26fdab50e0791ed278d343ee9521df7a993b2e8
Алдияр Макимов <makimovaldiarsamatovich@gmail.com>
2022-11-14 11:52:00 +0500

    12 13

---
+++ pz_12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {
+        static void Invert(int a, out int b)
+        {
+            string a1 = a.ToString();
+            char[] chars = a1.ToCharArray();
+            char[] chars1 = new char[chars.Length];
+            for (int i = 0; i < a1.Length; i++)
+            {
+                chars1[i] = chars[a1.Length - 1 - i];
+            }
+            b = Convert.ToInt32(new String(chars1));
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 1;
+            Invert(a, out b);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ pz_13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static int Factorial(int a)
+        {
+            int result = 1;
+            if (a == 0)
+            {
+                result = 1;
+            }
+            if (a < 0)
+            {
+                result = -1;
+            }
+            else
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    result = result * i;
+                }
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(a));
+        }
+
+    }
+}
